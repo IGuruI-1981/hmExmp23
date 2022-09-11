9c457f96a08cc3a11faed17834894e2185846140
Viktor <viktor.ihtiyarov@gmail.com>
2022-09-11 12:38:06 +0700

    commit1

---
+++ src/MeansOfTransport.java
@@ -0,0 +1,18 @@
+public class MeansOfTransport {
+
+    private String modelName;
+    private int wheelsCount;
+
+    public MeansOfTransport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+}
